057923ad2061138d41da05ea962c58032a086bec
AlvyneZ <alvynew@gmail.com>
2022-12-09 00:09:21 +0300

    Printing to screen

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    const char *str = "\"Programming is like building a multilingual puzzle\n";
+    puts(str);
+    return 0;
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+	char *str = "with proper grammar, but the outcome is a piece of art,\n";
+	printf(str);
+	return 0;
+}
