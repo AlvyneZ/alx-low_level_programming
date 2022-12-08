660ff72a27321aa96a87853f963e7ecdf1bfccd7
AlvyneZ <alvynew@gmail.com>
2022-12-09 00:32:06 +0300

    Parenthesis on output

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,15 +7,10 @@
  */
 int main()
 {
-	char a;
-	int b;
-	long c;
-	long long d;
-	float e;
-	printf("Size of a char: %d byte(s)\n", (int)sizeof(a));
-	printf("Size of an int: %d byte(s)\n", (int)sizeof(b));
-	printf("Size of a long int: %d byte(s)\n", (int)sizeof(c));
-	printf("Size of a long long int: %d byte(s)\n", (int)sizeof(d));
-	printf("Size of a float: %d byte(s)\n", (int)sizeof(e));
-	return 0;
+	printf("Size of a char: %d byte(s)\n", (int)sizeof(char));
+	printf("Size of an int: %d byte(s)\n", (int)sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", (int)sizeof(long));
+	printf("Size of a long long int: %d byte(s)\n", (int)sizeof(long long));
+	printf("Size of a float: %d byte(s)\n", (int)sizeof(float));
+	return (0);
 }
