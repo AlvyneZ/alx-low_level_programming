50dbd97086d65c5be58c5747a8351fffad92d34d
AlvyneZ <alvynew@gmail.com>
2022-12-13 21:45:55 +0300

    Printing minutes

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * jack_bauer- Print all the minutes of an hour
+ */
+void jack_bauer(void)
+{
+	int h1;
+	int h0;
+	int m0;
+	int m1;
+
+	for (h1 = 0; h1 < 3; h1++)
+	{
+		for (h0 = 0; ((h0 < 10) && !((h1 >= 2) && (h0 > 4))); h0++)
+		{
+			for (m1 = 0; m1 < 7; m1++)
+			{
+				for (m0 = 0; m0 < 10; m0++)
+				{
+					_putchar('0' + h1);
+					_putchar('0' + h0);
+					_putchar('0' + m1);
+					_putchar('0' + m0);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
