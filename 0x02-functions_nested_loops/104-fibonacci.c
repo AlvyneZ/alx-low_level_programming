5b8387948092f0e762baab9ff75831f61b98144c
AlvyneZ <alvynew@gmail.com>
2022-12-14 23:23:48 +0300

    Pseudo long long implementation

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,19 +7,32 @@
 int main(void)
 {
 	int cnt;
+	unsigned int prevXtra;
+	unsigned int curXtra;
+	unsigned int tmpXtra;
 	unsigned long prev;
 	unsigned long cur;
 	unsigned long  tmp;
 
+	prevXtra = 0;
 	prev = 1;
 	printf("%lu", prev);
+	curXtra = 0;
 	cur = 2;
 	for (cnt = 1; cnt < 98; cnt++)
 	{
-		printf(", %lu", cur);
+		if (curXtra)
+			printf(", %d%lu", curXtra, cur);
+		else
+			printf(", %lu", cur);
 		tmp = prev + cur;
+		tmpXtra = prevXtra + curXtra;
+		if ((tmp < prev) || (tmp < cur))
+			tmpXtra++;
 		prev = cur;
+		prevXtra = curXtra;
 		cur = tmp;
+		curXtra = tmpXtra;
 	}
 	printf("\n");
 
