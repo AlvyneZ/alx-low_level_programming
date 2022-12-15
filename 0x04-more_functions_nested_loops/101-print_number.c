f33b11a8bb3149f4b442bd87eddd8441e7ee57ec
AlvyneZ <alvynew@gmail.com>
2022-12-15 23:27:17 +0300

    Printing numbers

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_number - Print a multiple digit number including negative sign
+ * @n: Number to print
+ */
+void print_number(int n)
+{
+	int max;
+	int zeros;
+
+	zeros = 0;
+    if (n < 0)
+    {
+        _putchar('-');
+        n = -n;
+    }
+	for (max = 1000000000; max >= 1; max /= 10)
+	{
+		if (max == 1)
+		{
+			zeros = 1;
+		}
+		if (n < max)
+		{
+			if (zeros)
+				_putchar('0');
+		}
+		else
+		{
+			zeros = 1;
+			_putchar('0' + (n / max));
+			n %= max;
+		}
+	}
+}
