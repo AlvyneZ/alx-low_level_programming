55cc9cd3c5ec36bc1dbee9c766611cd91d7f063e
AlvyneZ <alvynew@gmail.com>
2022-12-16 00:09:38 +0300

    Prime factor calculation

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,43 @@
+# include <stdio.h>
+# include <math.h>
+
+/**
+ * biggest_prime - Gets the biggest prime factor of a number
+ * @n: Number whose prime factor is calculated
+ * Return: Biggest prime factor
+ */
+long biggest_prime(long n)
+{
+	long b;
+	long i;
+
+	b = 0;
+	while (n%2 == 0)
+	{
+		b = 2;
+		n /= 2;
+	}
+	for (i = 3; i <= sqrt(n); i = i+2)
+	{
+		while (n%i == 0)
+		{
+			b = i;
+			n /= i;
+		}
+	}
+	if (b == 0)
+	{
+		b = n;
+	}
+	return (b);
+}
+
+/**
+ * main - Prints the biggest prime factor of 612852475143
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%ld\n", biggest_prime(612852475143));
+	return (0);
+}
