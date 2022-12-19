8f5d5f475c9c8740718dea373003a3a40826c702
AlvyneZ <alvynew@gmail.com>
2022-12-19 23:43:51 +0300

    Generating random password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,5 +1,6 @@
-#include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * main - Print a password
@@ -8,6 +9,20 @@
  */
 int main(void)
 {
-	printf("Password");
+	int ind, len;
+	char c;
+
+	/* Use current time as seed for random generator */
+	srand(time(0));
+
+	len = rand() % 100;
+	for (ind = 0; ind < len; ind++)
+	{
+		if ((rand() % 100) > 50)
+			c = 'a' + (rand() % 26);
+		else
+			c = 'A' + (rand() % 26);
+		printf("%c", c);
+	}
 	return (0);
 }
