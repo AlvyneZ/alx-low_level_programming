0bcf320ef488625f9b66bde5051c969a36ed40ef
AlvyneZ <alvynew@gmail.com>
2022-12-19 23:07:55 +0300

    Converting string to integer (atoi implementation)

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+* _isdigit - Checks if a character is in a digit
+ * @c: Character to be checked
+ * Return: 1 if it is a digit & 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if ((c >= '0') && (c <= '9'))
+		return (1);
+	return (0);
+}
+
+/**
+* _atoi - Converts a string to an integer
+ * @s: String to be converted
+ * Return: resulting integer value
+ */
+int _atoi(char *s)
+{
+	int ret, sign, start;
+
+	start = 0;
+	for (ret = 0, sign = 1; (*s != '\0'); s++)
+	{
+		if (_isdigit(*s))
+		{
+			start = 1;
+			ret *= 10;
+			ret += ((*s) - '0');
+		}
+		else if (start)
+		{
+			break;
+		}
+		else if (*s == '-')
+		{
+			sign *= -1;
+		}
+	}
+	return (ret * sign);
+}
