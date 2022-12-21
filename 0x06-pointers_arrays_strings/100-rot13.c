439fc8f12cbce29984ee51c48f5391e4387563ee
AlvyneZ <alvynew@gmail.com>
2022-12-21 14:04:56 +0300

    Rot13 implementation with 1 if and 2 loops only

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rot13 - Encodes a string using rot13
+ * @s: String to be edited
+ * Return: Edited string
+ */
+char *rot13(char *s)
+{
+	char *sCurs;
+
+	for (sCurs = s; (*sCurs != '\0'); sCurs++)
+	{
+		if (((*sCurs >= 'n') && (*sCurs <= 'z')) ||
+		((*sCurs >= 'N') && (*sCurs <= 'Z')))
+		{
+			*sCurs -= 26;
+		}
+		while (((*sCurs >= ('a' - 13)) && (*sCurs <= 'm')) ||
+		((*sCurs >= ('A' - 13)) && (*sCurs <= 'M')))
+		{
+			*sCurs += 13;
+			break;
+		}
+	}
+	return (s);
+}
