459fae911a4ff42a641933bcb92de1821d6b409d
AlvyneZ <alvynew@gmail.com>
2022-12-22 21:37:32 +0300

    Refactored code

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,20 +1,6 @@
 #include "main.h"
 
 /**
-* _strlen - Gets the length of a string
- * @s: String whose length is required
- * Return: Length of string s
- */
-int _strlen(char *s)
-{
-	int ind;
-
-	for (ind = 0; (*(s + ind) != '\0'); ind++)
-	;
-	return (ind);
-}
-
-/**
 * reverse_array - Reverses an integer array
  * @a: Array to be reversed
  * @n: Number of elements in the array
