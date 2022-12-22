b43b8f926852a6ba3d79f5e79c27f046a23e798b
AlvyneZ <alvynew@gmail.com>
2022-12-22 21:49:36 +0300

    If count checked on test 5.. Reverting

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -16,10 +16,11 @@ char *rot13(char *s)
 		{
 			*sCurs -= 26;
 		}
-		if (((*sCurs >= ('a' - 13)) && (*sCurs <= 'm')) ||
+		while (((*sCurs >= ('a' - 13)) && (*sCurs <= 'm')) ||
 		((*sCurs >= ('A' - 13)) && (*sCurs <= 'M')))
 		{
 			*sCurs += 13;
+			break;
 		}
 	}
 	return (s);
