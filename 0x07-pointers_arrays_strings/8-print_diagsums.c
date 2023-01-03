1bec3cd218db0f844538856e4fb8b3ce54b70994
AlvyneZ <alvynew@gmail.com>
2023-01-03 22:04:05 +0300

    Investigating array storage

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,98 @@
+#include "main.h"
+
+/**
+ * print_negative - Print a negative multiple digit number
+ * @n: Number to print
+ * Return: 1 if number is negative, 0 otherwise
+ */
+int print_negative(int n)
+{
+	int max;
+	int zeros;
+
+	zeros = 0;
+	if (n < 0)
+	{
+		_putchar('-');
+		for (max = -1000000000; max <= -1; max /= 10)
+		{
+			if (max == -1)
+			{
+				zeros = 1;
+			}
+			if (n > max)
+			{
+				if (zeros)
+					_putchar('0');
+			}
+			else
+			{
+				zeros = 1;
+				_putchar('0' + (n / max));
+				n %= (-max);
+			}
+		}
+		return (1);
+	}
+	return (0);
+}
+
+/**
+ * print_number - Print a multiple digit number including negative sign
+ * @n: Number to print
+ */
+void print_number(int n)
+{
+	int max;
+	int zeros;
+
+	zeros = 0;
+	if (!(print_negative(n)))
+	{
+		for (max = 1000000000; max >= 1; max /= 10)
+		{
+			if (max == 1)
+			{
+				zeros = 1;
+			}
+			if (n < max)
+			{
+				if (zeros)
+					_putchar('0');
+			}
+			else
+			{
+				zeros = 1;
+				_putchar('0' + (n / max));
+				n %= max;
+			}
+		}
+	}
+}
+
+/**
+ * print_diagsums - Prints the sum of the two diagonals of a
+ *   square matrix of integers
+ * @a: Memory area of square matrix
+ * @size: Size of matrix
+ */
+void print_diagsums(int *a, int size)
+{
+	int curs, sum;
+
+	sum = 0;
+	for (curs = 0; curs < (size * size); curs += size + 1)
+	{
+		sum += a[curs];
+	}
+	print_number(sum);
+	_putchar(',');
+	_putchar(' ');
+	sum = 0;
+	for (curs = size - 1; curs < (size * size); curs += size - 1)
+	{
+		sum += a[curs];
+	}
+	print_number(sum);
+	_putchar('\n');
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,6 +2,8 @@
 #define MAIN_H
 
 int _putchar(char c);
+void print_number(int n);
+int print_negative(int n);
 
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
@@ -10,5 +12,6 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
 
 #endif /* MAIN_H */
