4ac72506b73b77c4124634a907e6c77e5c8cce7e
AlvyneZ <alvynew@gmail.com>
2023-01-05 22:10:53 +0300

    sqrt implementation

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _sqrt_finder - Recursively 
+ * @n: Number to be square-rooted
+ * @a: Guess of square-root
+ * Return: Number closer to Square-root of n
+ */
+int _sqrt_finder(int n, int a)
+{
+	int t;
+
+	if ((n == 0) || (n == 1))
+		return (n);
+	if (a == 0)
+		return (-1);
+	t = n / a;
+	if (t == a)
+	{
+		if ((a * a) == n)
+			return (a);
+		else
+			return (-1);
+	}
+	return (_sqrt_finder(n, ((t + a) / 2)));
+}
+
+/**
+ * _sqrt_recursion - Returns the natural square-root of a number
+ * @n: Number to be square-rooted
+ * Return: Square-root of n
+ */
+int _sqrt_recursion(int n)
+{
+	return (_sqrt_finder(n, n / 2));
+}
