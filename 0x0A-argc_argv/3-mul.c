77e16ded1788536b72da7be39010b37754858192
AlvyneZ <alvynew@gmail.com>
2023-01-09 19:38:51 +0300

    Accounting for non-digit args

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,9 +12,22 @@ int main(int argc, char *argv[])
 	int mul, a, b;
 
 	if (argc < 3)
+	{
 		printf("Error\n");
+		return(0);
+	}
 	a = atoi(argv[1]);
+	if ((a == 0) && (argv[1] != "0"))
+	{
+		printf("Error\n");
+		return(0);
+	}
 	b = atoi(argv[2]);
+	if ((b == 0) && (argv[2] != "0"))
+	{
+		printf("Error\n");
+		return(0);
+	}
 	mul = a * b;
 	printf("%d\n", mul);
 	return (0);
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,11 +9,19 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, sum;
+	int i, sum, a;
 
 	sum = 0;
 	for (i = 1; i < argc; i++)
-		sum += atoi(argv[i]);
+	{
+		a = atoi(argv[i]);
+		if ((a == 0) && (argv[i] != "0"))
+		{
+			printf("Error\n");
+			return(0);
+		}
+		sum += a;
+	}
 	printf("%d\n", sum);
 	return (0);
 }
