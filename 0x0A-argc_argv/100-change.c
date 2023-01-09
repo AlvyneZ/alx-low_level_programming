2725940dbad97b170318c1df82cc9162e1fd871c
AlvyneZ <alvynew@gmail.com>
2023-01-09 20:11:22 +0300

    Accounting for negatives

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -3,7 +3,7 @@
 
 /**
  * main- Calculates and prints the minimum number of
- *  coins ( 25, 10, 5, 2, and 1) needed to make change
+ *  coins (25, 10, 5, 2, and 1) needed to make change
  *  for an amount of money provided in command-line
  * @argc: Number of command-line arguments
  * @argv: Array of command-line arguments
@@ -13,15 +13,21 @@ int main(int argc, char *argv[])
 {
 	const int coins[5] = {25, 10, 5, 2, 1};
 	int i, sum, a;
+	char *endptr;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (0);
 	}
-	a = atoi(argv[1]);
+	a = (int)strtol(argv[1], &endptr, 10);
+	if (*endptr != '\0')
+	{
+		printf("Error\n");
+		return (0);
+	}
 	sum = 0;
-	for (i = 0; i < 5; i++)
+	for (i = 0; ((i < 5) && (a > 0)); i++)
 	{
 		sum += (a / coins[i]);
 		a = a % coins[i];
