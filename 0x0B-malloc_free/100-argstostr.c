ff61dce7414dbd3495d1bc55883123e2095f0e20
AlvyneZ <alvynew@gmail.com>
2023-01-10 23:34:56 +0300

    Concatenating multiple strings

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,63 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* _strlen - Gets the length of a string
+ * @s: String whose length is required
+ * Return: Length of string s
+ */
+int _strlen(char *s)
+{
+	int ind;
+
+	for (ind = 0; (*(s + ind) != '\0'); ind++)
+	;
+	return (ind);
+}
+
+/**
+* _strcpy - Copies a string to another string
+ * @dest: Destination to paste string
+ * @src: Source string to copy from
+ * Return: destination string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *destCurs, *srcCurs;
+
+	srcCurs = src;
+	destCurs = dest;
+	for (; (*srcCurs != '\0'); srcCurs++, destCurs++)
+		*destCurs = *srcCurs;
+	*destCurs = '\0';
+
+	return (dest);
+}
+
+/**
+ * argstostr - Concatenates multiple strings into a new string
+ * @ac: Number of strings
+ * @av: Array of Strings
+ * Return: a pointer to the concatenated string, or NULL if
+ *  it fails
+ */
+char *argstostr(int ac, char **av)
+{
+	char *out;
+	int len, ind;
+
+	if ((av == NULL) || (ac == 0))
+		return (NULL);
+	for (ind = 0, len = 0; ind < ac; ind++)
+		len += 1 + _strlen(av[ind]);
+	out = malloc(len);
+	if (out == NULL)
+		return (NULL);
+	for (ind = 0, len = 0; ind < ac; ind++)
+	{
+		_strcpy(&(out[len]), av[ind]);
+		len += _strlen(av[ind]);
+		out[len++] = '\n';
+	}
+	return (out);
+}
