67b8185dcccf05a4fd98f7438cfeb1403528a2c1
AlvyneZ <alvynew@gmail.com>
2023-01-10 22:29:24 +0300

    Removed unused _memset

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -2,25 +2,6 @@
 #include <stdlib.h>
 
 /**
- * _memset - Fills the first n bytes of the memory area pointed
- *   to by s with the constant byte b
- * @s: Memory area to be edited
- * @b: Byte to be filled into the memory area
- * @n: bytes to be filled
- * Return: Edited memory area
- */
-char *_memset(char *s, char b, unsigned int n)
-{
-	unsigned int curs;
-
-	for (curs = 0; curs < n; curs++)
-	{
-		s[curs] = b;
-	}
-	return (s);
-}
-
-/**
  * alloc_grid - Creates a 2D grid of integers of specified
  *  size, and filled with zeros
  * @width: Array width
