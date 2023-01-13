2d0be52e696098d1410cd9349ca8732b59f2289f
AlvyneZ <alvynew@gmail.com>
2023-01-14 00:00:10 +0300

    Fixed typing errors

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -2,25 +2,6 @@
 #include <stdlib.h>
 
 /**
- * _memset - Fills the first n bytes of the memory area pointed
- *   to by s with the constant byte b
- * @s: Memory area to be edited
- * @b: Byte to be filled into the memory area
- * @n: bytes to be filled
- * Return: Edited memory area
- */
-char *_memset(char *s, char b, unsigned int n)
-{
-	unsigned int curs;
-
-	for (curs = 0; curs < n; curs++)
-	{
-		s[curs] = b;
-	}
-	return (s);
-}
-
-/**
  * array_range - Allocates memory dynamically for an
  *  array initialized with a specified range of numbers
  * @min: Start of range
@@ -31,16 +12,16 @@ char *_memset(char *s, char b, unsigned int n)
 int *array_range(int min, int max)
 {
 	void *mem;
-	char *curs;
+	int *curs;
 
 	if (min > max)
 		return (NULL);
 	mem = malloc(max + 1 - min);
 	if (mem == NULL)
 		return (NULL);
-	for (curs = (char *)mem; min <= max; curs++)
+	for (curs = (int *)mem; min <= max; curs++)
 	{
-		curs = min++;
+		*curs = min++;
 	}
 	return (mem);
 }
