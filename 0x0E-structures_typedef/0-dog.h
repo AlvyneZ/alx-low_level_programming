aa58dd7d93f111e1d8f2e3df03bcdc81571836fd
AlvyneZ <alvynew@gmail.com>
2023-01-17 22:11:28 +0300

    Creating a struct

---
+++ 0x0E-structures_typedef/0-dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_HEADER
+#define DOG_HEADER
+
+/**
+ * struct dog - Details of a dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_HEADER */
