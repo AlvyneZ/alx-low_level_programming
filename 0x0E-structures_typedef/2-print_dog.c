f406e7d651dfc4a369e9140a531879bd3e08a581
AlvyneZ <alvynew@gmail.com>
2023-01-17 22:40:25 +0300

    Added newline character at end

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,9 +9,9 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	printf("Name: %s", ((d->name == NULL) ?
+	printf("Name: %s\n", ((d->name == NULL) ?
 		"(nil)" : d->name));
-	printf("Age: %f", d->age);
-	printf("Owner: %s", ((d->owner == NULL) ?
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", ((d->owner == NULL) ?
 		"(nil)" : d->owner));
 }
