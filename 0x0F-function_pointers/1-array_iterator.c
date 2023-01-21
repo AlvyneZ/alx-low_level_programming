88e22d2fea1cd11aded5533ff7e99849dd8dafef
AlvyneZ <alvynew@gmail.com>
2023-01-21 19:33:12 +0300

    Bug fix

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,7 +12,7 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	if ((array == NULL) || (action == NULL) || (size == NULL))
+	if ((array == NULL) || (action == NULL) || (size == 0))
 		return;
 	do
 	{
