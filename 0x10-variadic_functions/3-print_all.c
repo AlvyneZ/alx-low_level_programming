e5c1b32e04757e9d5a33165466dddd4c80f6baec
AlvyneZ <alvynew@gmail.com>
2023-01-22 13:21:58 +0300

    Simplified printf implementation

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,5 +1,50 @@
 #include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include <stdarg.h>
+
+/**
+ * print_int- Prints an integer from the first element of
+ *  a given variable list
+ * @var: Pointer to the variable list
+ */
+void print_int(va_list *var)
+{
+	printf("%d", va_arg(*var, int));
+}
+
+/**
+ * print_float- Prints a float from the first element of
+ *  a given variable list
+ * @var: Pointer to the variable list
+ */
+void print_float(va_list *var)
+{
+	printf("%f", va_arg(*var, double));
+}
+
+/**
+ * print_char- Prints a char from the first element of
+ *  a given variable list
+ * @var: Pointer to the variable list
+ */
+void print_char(va_list *var)
+{
+	printf("%c", va_arg(*var, int));
+}
+
+/**
+ * print_str- Prints a string from the first element of
+ *  a given variable list
+ * @var: Pointer to the variable list
+ */
+void print_str(va_list *var)
+{
+	char *str, *nil = "(nil)";
+
+	str = va_arg(*var, char *);
+	printf("%s", (str == NULL) ? nil : str);
+}
 
 /**
  * print_all - Prints all its parameters (any type) given
@@ -8,5 +53,31 @@
  */
 void print_all(const char * const format, ...)
 {
-	
+	unsigned int i, j;
+	va_list var;
+	format_spec_t form[] = {
+		{'c', &print_char},
+		{'i', &print_int},
+		{'f', &print_float},
+		{'s', &print_str}
+	};
+
+	va_start(var, format);
+	i = 0;
+	while ((format != NULL) && (format[i] != '\0'))
+	{
+		j = 0;
+		while (j < 4)
+		{
+			if (format[i] == form[j].spec)
+			{
+				form[j].print_func(&var);
+				printf(", ");
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(var);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,21 @@
 #ifndef VAR_FUNC_HEADER
 #define VAR_FUNC_HEADER
 
+#include <stdarg.h>
+
+typedef void (*print_func_t)(va_list *);
+
+/**
+ * struct format_spec_s - Format specifier details
+ * @spec: Type Specifier character
+ * @print_func: Function to print the given type
+ */
+typedef struct format_spec_s
+{
+	char spec;
+	print_func_t print_func;
+} format_spec_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
