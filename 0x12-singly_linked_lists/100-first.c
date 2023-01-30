6447762a9641401b77bd7c49a743d459697cf409
AlvyneZ <alvynew@gmail.com>
2023-01-30 22:12:08 +0300

    Executing a function before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * constructor_func - Prints some text before main runs
+ */
+void constructor_func(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
