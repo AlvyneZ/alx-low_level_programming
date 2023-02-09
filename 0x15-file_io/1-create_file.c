59585ce63039eb0bc7c4582fbcc7bb7c1f9c4f90
AlvyneZ <alvynew@gmail.com>
2023-02-09 20:52:45 +0300

    Opening with write only

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,7 +28,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	filde = open(filename, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+	filde = open(filename, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
 	if (filde < 0)
 		return (-1);
 	if (text_content == NULL)
