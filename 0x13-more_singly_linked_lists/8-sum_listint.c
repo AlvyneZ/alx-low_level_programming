00e90e71aeeac15c7c938038cdef8b3d8947b5b2
AlvyneZ <alvynew@gmail.com>
2023-02-13 20:36:20 +0300

    Fixed bug

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -9,7 +9,7 @@ int sum_listint(listint_t *head)
 {
 	int sum;
 
-	for (sum = 0; head != NULL; head = head->next, sum++)
+	for (sum = 0; head != NULL; head = head->next)
 		sum += head->n;
 	return (sum);
 }
