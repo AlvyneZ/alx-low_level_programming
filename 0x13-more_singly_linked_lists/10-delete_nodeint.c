669009c0bb203b5e6df98e3346a8315d8cefa881
AlvyneZ <alvynew@gmail.com>
2023-02-13 21:07:51 +0300

    Accounting for empty list

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -20,7 +20,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		prev = curs;
 		curs = curs->next;
 	}
-	if (cnt != index)
+	if ((cnt != index) || (curs == NULL))
 	{
 		return (-1);
 	}
