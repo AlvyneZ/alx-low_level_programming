718fb4c293bf9f7cc82bea34554bfbceaa0c2cc1
AlvyneZ <alvynew@gmail.com>
2023-02-13 20:08:44 +0300

    reverting to no print for empty list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,12 +11,6 @@ size_t print_listint(const listint_t *h)
 	size_t cnt;
 
 	for (cnt = 0; h != NULL; h = h->next, cnt++)
-	{
 		printf("%d\n", h->n);
-	}
-	if (cnt == 0)
-	{
-		printf("\n");
-	}
 	return (cnt);
 }
