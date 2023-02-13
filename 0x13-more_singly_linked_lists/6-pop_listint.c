2c026a9bb16853654eaff9248fe359b2bf834275
AlvyneZ <alvynew@gmail.com>
2023-02-13 19:59:10 +0300

    Popping from linked list

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * pop_listint - Deletes head node and retrieves its data
+ * @head: List to be manipulated
+ * Return: data stored in removed head node
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *node;
+	int n;
+
+	if (head == NULL)
+		return (0);
+	node = (*head);
+	if (node == NULL)
+		return (0);
+	n = node->n;
+	node = node->next;
+	(*head) = node;
+	return (n);
+}
