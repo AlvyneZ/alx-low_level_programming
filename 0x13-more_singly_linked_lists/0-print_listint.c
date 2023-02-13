d14252245e781a207ae27c3bb222d52493321e57
AlvyneZ <alvynew@gmail.com>
2023-02-13 21:18:01 +0300

    Testing

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,6 +11,6 @@ size_t print_listint(const listint_t *h)
 	size_t cnt;
 
 	for (cnt = 0; h != NULL; h = h->next, cnt++)
-		printf("%d\n", h->n);
+		printf("%d\n", (h->n));
 	return (cnt);
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef SL_LISTS_HEADER
-#define SL_LISTS_HEADER
+#ifndef SL_LISTS_INT_HEADER
+#define SL_LISTS_INT_HEADER
 
 #include <stdlib.h>
 
@@ -31,4 +31,4 @@ int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 
-#endif /* SL_LISTS_HEADER */
+#endif /* SL_LISTS_INT_HEADER */
