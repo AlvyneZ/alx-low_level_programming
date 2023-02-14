a6ca6bb1c1495a8dadc6a23376e428ca6540e796
AlvyneZ <alvynew@gmail.com>
2023-02-14 20:57:24 +0300

    Fixing bugs

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+#define NULL ((void *)0)
+
 /**
  * binary_to_uint - Converts a binary number to an unsigned int
  * @b: String of 0's and 1's of the binary number
@@ -9,6 +11,8 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int i;
 
+	if (b == NULL)
+		return (0);
 	for (i = 0; (*b) != '\0'; b++)
 	{
 		i *= 2;
--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,6 +8,8 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	if (index > ((sizeof(unsigned long int) * 8) - 1))
+		return (-1);
 	n >>= index;
 	if (n & 1UL)
 		return (1);
