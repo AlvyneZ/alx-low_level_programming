93a41394caa3add022ed4a9ad4ad1a9ea43d56f6
AlvyneZ <alvynew@gmail.com>
2023-04-07 01:52:49 +0300

    Validating function arguments (NULL is invalid)

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -15,6 +15,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int idx;
 	hash_node_t *node;
 
+	if ((ht == NULL) || (key == NULL) || (strlen(key) == 0))
+		return (NULL);
+
 	idx = key_index((const unsigned char *)key, ht->size);
 
 	for (node = ht->array[idx]; node != NULL; node = node->next)
